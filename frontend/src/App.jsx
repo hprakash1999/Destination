ca318242c18f479bdd11f902bb4788a5d06fe106
Harsh Prakash <harshprakash1999@gmail.com>
2024-10-05 18:07:11 +0530

    Render (Layout) component in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,21 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
+// Import components
+import { Layout } from "./components/Components.js";
+
+// Create router
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<Layout />}></Route>)
+);
+
+// Render router
 function App() {
-  return <div>Hello user. Welcome to destination!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
