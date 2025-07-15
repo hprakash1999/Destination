8e2f273fddbfe26f858c0fc407a51921f4320cd9
Harsh Prakash <harshprakash1999@gmail.com>
2025-07-15 21:48:37 +0530

    Update bugs

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios instance with default configuration
 const api = axios.create({
-  baseURL: "/api/v1",
+  baseURL: "http://localhost:5001/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
