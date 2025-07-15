278e30c7eba2dd933541cd4b5883146c0b6783b3
Harsh Prakash <harshprakash1999@gmail.com>
2025-07-15 21:58:47 +0530

    Update Api url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Axios instance with default configuration
 const api = axios.create({
-  baseURL: "http://localhost:5001/api/v1",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   headers: {
     "Content-Type": "application/json",
   },
