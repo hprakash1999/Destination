67a927b36b164dcd7cde5ea63e5e9744b84ad37c
Harsh Prakash <harshprakash1999@gmail.com>
2025-07-16 13:16:20 +0530

    Update cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import express from "express";
 const app = express();
 
 // Configuring CORS
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: `${process.env.CORS_ORIGIN}`, credentials: true }));
 
 // Configuring Express
 app.use(express.json({ limit: "20kb" }));
